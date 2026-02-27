cb48ef6b7973075c8ffe7374ad7b63d6c1b550c1
krlavanyabca06 <krlavanyabca25@rvu.edu.in>
2026-02-27 12:04:28 +0530

    Add files via upload

---
+++ lab3.sh
@@ -0,0 +1,22 @@
+echo "job1:Display Date"
+date
+sleep 3
+
+echo "job2:Display current user"
+whoami
+sleep 3
+
+echo "job3:Display Calender"
+cal
+sleep 2 
+
+echo "job4:List the files"
+ls
+sleep 3 
+
+echo "job5:Display my system date"
+date +"%T"
+sleep 1
+
+echo "job6"
+pwd
